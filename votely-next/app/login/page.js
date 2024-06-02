d86147e3c2c45021aa3cfa55b0fc0ee04f1dcf85
arsyadal <arsyadal@student.telkomuniversity.ac.id>
2024-06-02 18:03:43 +0700

    first commit

---
+++ votely-next/app/login/page.js
@@ -0,0 +1,54 @@
+export default function Login() {
+  return (
+    <div>
+      <link href="https://cdn.jsdelivr.net/npm/daisyui@4.11.1/dist/full.min.css" rel="stylesheet" type="text/css" />
+      <script src="https://cdn.tailwindcss.com"></script>
+      <div class="navbar bg-black text-white">
+        <div class="navbar-start">
+          <div class="dropdown"> </div>
+          <a href="/homepage" class="btn btn-ghost text-xl">
+            VOTELY
+          </a>
+        </div>
+      </div>
+
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <div class="text-center lg:text-left">
+            <h1 class="text-5xl font-bold text-black">Login With SSO!</h1>
+            <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          </div>
+          <div class="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form class="card-body">
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">Email</span>
+                </label>
+                <input type="email" placeholder="email" class="input input-bordered" required />
+              </div>
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">Password</span>
+                </label>
+                <input type="password" placeholder="password" class="input input-bordered" required />
+                <label class="label">
+                  <a href="#" class="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                  <a href="/register" class="label-text-alt link link-hover">
+                    Dont have account?
+                  </a>
+                </label>
+              </div>
+              <div class="form-control mt-6">
+                <a href="/homepage" class="btn bg-black text-white">
+                  Login
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
